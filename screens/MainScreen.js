be474c0b9c2b960b155354f26fd3490aa6156785
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-11-02 13:30:40 +0200

    canvas toimii

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+
 import { View, Text, StyleSheet } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { SafeAreaView, Alert } from 'react-native';
+import Canvas from 'react-native-canvas';
 
 const MainScreen = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+      if (ref.current) {
+        const ctx = ref.current.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(100, 100, 40, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+      }
+    }, [ref]);
+
     return (
+        <>
         <View style={styles.container}>
-            <Text style={styles.text}>Main Screen</Text>
+            <Text style={styles.text}>Vatupassi</Text>
         </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Canvas style={{ width: '100%', height: '100%', backgroundColor: 'black' }} ref={ref} />
+        </SafeAreaView>
+
+        </>
     );
 };
 
