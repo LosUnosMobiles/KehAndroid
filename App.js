c18a69c8df1f6ff06e87bc7fd166d6a4dc840a83
Janne Paaso <jannepaaso@live.com>
2024-11-04 14:58:44 +0200

    Modified to complete

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SpiritLevelDisplay from './screens/testScreen';
+import MainScreen from "./screens/MainScreen";
 
 export default function App() {
   return (
-    <SpiritLevelDisplay />
+    <MainScreen />
   );
 }
 
